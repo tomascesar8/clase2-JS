fc2e7a1d577adf6be0f484c56bcea380f84b7886
Tomás César <tomascesar1993@gmail.com>
2022-07-18 08:28:27 -0300

    primera clase de JS

---
+++ js/app.js
@@ -0,0 +1,35 @@
+// let numero1='15';
+// let numero2='3';
+
+// console.log(numero1 + numero2);
+// console.log(numero1 * numero2);
+// console.log(numero1<10 || numero2==3 );
+// console.log(numero1<15 && numero2==3 );
+alert ("soy una alerta");
+
+let a = prompt ("ingresa un valor para a");
+let b = prompt ("ingresa un valor para b");
+
+let suma = (a+b);
+let multiplo = (a*b);
+
+console.log(suma);
+console.log(multiplo);
+
+suma >20 || multiplo >20 ? console.log("Hay un numero mayor a 20"): console.log("Hay un numero menor que 20");
+
+let suma1 = (suma++);
+let dismunuye1 = (--suma);
+
+console.log(suma1);
+console.log(dismunuye1);
+//!
+//?
+//*
+
+
+//xzzxczc
+
+/*zzxzczczxc
+zzxzcz
+zxcxzc*/
